a78915b16e6016003f594ce6de7a7683c8f59465
old-coot <knightsofthewharf@gmail.com>
2024-03-19 22:37:12 +0300

    3lession

---
+++ Assets/Scripts/Follow.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public Transform Target;
+    public float LerpRate;
+
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, Target.position, Time.deltaTime * LerpRate);
+    }
+}
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,20 +6,67 @@ public class PlayerMove : MonoBehaviour
     public float MoveSpeed;
     public float JumpSpeed;
     public float Friction;
+    public float MaxSpeed;
     public bool Grounded;
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Grounded)
         {
-            Rigidbody.AddForce(0, JumpSpeed, 0, ForceMode.VelocityChange);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Rigidbody.AddForce(0, JumpSpeed, 0, ForceMode.VelocityChange);
+            }
+
         }
     }
 
     private void FixedUpdate()
     {
-        Rigidbody.AddForce(Input.GetAxis("Horizontal") * MoveSpeed, 0, 0, ForceMode.Acceleration);
-        Rigidbody.AddForce(-Rigidbody.velocity.x * Friction, 0, 0, ForceMode.Acceleration);
+        float speedMultiplier = 1f;
+
+        if (Grounded == false)
+        {
+            speedMultiplier = 0.2f;
+        }
+
+        if (Rigidbody.velocity.x > MaxSpeed && Input.GetAxis("Horizontal") > 0)
+        {
+            speedMultiplier = 0;
+        }
+
+        if (Rigidbody.velocity.x < -MaxSpeed && Input.GetAxis("Horizontal") < 0)
+        {
+            speedMultiplier = 0;
+        }
+
+        Rigidbody.AddForce(Input.GetAxis("Horizontal") * MoveSpeed * speedMultiplier, 0, 0, ForceMode.Acceleration);
+
+        if (Grounded)
+        {
+            Rigidbody.AddForce(-Rigidbody.velocity.x * Friction, 0, 0, ForceMode.Acceleration);
+
+        }
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        float angle = Vector3.Angle(collision.contacts[0].normal, Vector3.up);
+        if (angle < 45)
+        {
+
+            Grounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Grounded = false;
+    }
+
 
 }
